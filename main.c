fb7a82c00ff9c43b8f0f592258f2fd8c902711e4
Jerfy <jeffreyhuangdragon@gmail.com>
2019-10-02 14:41:38 -0400

    added welcome screen

--- main.c
+++ main.c
@@ -19,10 +19,39 @@ void main(void){
         initADC();
         configBtn();
 
-        unsigned char currBtn;
+        unsigned int mode;
+        unsigned char currBtn, currKey;
 
         for(;;){
             ADC12CTL0 |= ADC12SC;
             currBtn = getBtn();
+
+            if(currBtn==BIT0){
+                mode = 1;
+                Graphics_clearDisplay(&g_sContext);
+                Graphics_drawStringCentered(&g_sContext, "Producing DC Voltage", AUTO_STRING_LENGTH, 48,48, OPAQUE_TEXT);
+                Graphics_flushBuffer(&g_sContext);
+            }else if(currBtn==BIT6){
+                mode = 2;
+                Graphics_clearDisplay(&g_sContext);
+                Graphics_drawStringCentered(&g_sContext, "Producing Square Wave", AUTO_STRING_LENGTH, 48,48, OPAQUE_TEXT);
+                Graphics_flushBuffer(&g_sContext);
+            }else if(currBtn==BIT2){
+                mode = 3;
+                Graphics_clearDisplay(&g_sContext);
+                Graphics_drawStringCentered(&g_sContext, "Producing Sawtooth Wave", AUTO_STRING_LENGTH, 48,48, OPAQUE_TEXT);
+                Graphics_flushBuffer(&g_sContext);
+            }else if(currBtn==BIT4){
+                mode = 4;
+                Graphics_clearDisplay(&g_sContext);
+                Graphics_drawStringCentered(&g_sContext, "Producing Triangle Wave", AUTO_STRING_LENGTH, 48,48, OPAQUE_TEXT);
+                Graphics_flushBuffer(&g_sContext);
+            }else{
+                Graphics_drawString(&g_sContext, "Button 1: DC", AUTO_STRING_LENGTH, 10,12, OPAQUE_TEXT);
+                Graphics_drawString(&g_sContext, "Button 2: Square Wave", AUTO_STRING_LENGTH, 10,24, OPAQUE_TEXT);
+                Graphics_drawString(&g_sContext, "Button 3: Sawtooth Wave", AUTO_STRING_LENGTH, 10,36, OPAQUE_TEXT);
+                Graphics_drawString(&g_sContext, "Button 4: Triangle Wave", AUTO_STRING_LENGTH, 10,48, OPAQUE_TEXT);
+                Graphics_flushBuffer(&g_sContext);
+            }
         }
 }
